3700151764932db54d5c11b6c660fbea5c0638f3
Huy Pham <huypham.rko@gmail.com>
2023-04-10 08:27:01 +0700

    update wlistss

--- components/productItem/ProductItem.jsx
+++ components/productItem/ProductItem.jsx
@@ -118,7 +118,7 @@ const ProductItem = ({ products, tab, disp, wishlist, cart, qty }) => {
                   </button>
                 </li>
                 <li>
-                  {wishlist.find((item) => item._id == products._id) ? (
+                  {wishlist ? (
                     <button
                       className="nls-add-wishlist active"
                       type="button"
